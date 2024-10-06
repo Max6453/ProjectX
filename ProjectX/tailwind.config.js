d76f3c89a4a42768e7b598755ca6d73816325efd
Max6453 <146641020+Max6453@users.noreply.github.com>
2024-10-06 15:05:20 +0200

    Create tailwind.config.js

---
+++ ProjectX/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".index.html",
+            "./assets/src/*.{css, js}",
+            "./JS/*.{js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
