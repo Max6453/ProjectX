920cf8a1eed65bea6f99c8f8f83a4780b35b5b8d
Max6453 <146641020+Max6453@users.noreply.github.com>
2024-12-09 13:27:29 +0100

    git commit

--- connect.php
+++ connect.php
@@ -1,3 +1,4 @@
+<_php
 $host="127.0.0.1";
 $port=3306;
 $socket="";
@@ -9,3 +10,4 @@ $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
 
 //$con->close();
+>
